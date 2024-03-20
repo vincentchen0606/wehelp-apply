3832145a37239e2f4189e9742caa0b8e4f0d2ed7
vincentchen66 <153744323+vincentchen0606@users.noreply.github.com>
2024-03-20 11:14:51 +0800

    Add files via upload
    點擊標題，開合內容

--- answer.js
+++ answer.js
@@ -6,51 +6,29 @@
 //     answer.style.display = "none";
 //   }
 // }
-// window.onload = function () {
-//   const collapsibleSections = document.querySelectorAll(".card");
-
-//   collapsibleSections.forEach(function (card) {
-//     const collapseContent = document.querySelector(".collapse-content");
-//     const iconButton = document.querySelector(".icon-button");
-//     const icon = document.querySelector(".icon");
-
-//     function toggleCollapse() {
-//       collapseContent.classList.toggle("open");
-//       icon.classList.toggle("rotate");
-//     }
-//   });
-//   iconButton.addEventListener("click", toggleCollapse);
-// };
 //
-
 // window.onload = function () {
-//   const collapsibleSections = document.querySelectorAll(".card");
-//   collapsibleSections.forEach(function (card) {
-//     const collapseContent = card.querySelector(".collapse-content");
-//     const iconButton = card.querySelector(".icon-button");
-//     const icon = card.querySelector(".icon");
+//   const collapsibleSections = document.querySelectorAll("title1");
+//   collapsibleSections.forEach(function (title1) {
+//     const collapseContent = title1.querySelector(".collapse-content");
 
-//     function toggleCollapse() {
+//     function toggleCollapse(event) {
+//       //   event.stopPropagation(); // 阻止事件冒泡
 //       collapseContent.classList.toggle("open");
-//       icon.classList.toggle("rotate");
 //     }
+
 //     iconButton.addEventListener("click", toggleCollapse);
 //   });
 // };
-
 window.onload = function () {
-  const collapsibleSections = document.querySelectorAll(".card");
-  collapsibleSections.forEach(function (card) {
-    const collapseContent = card.querySelector(".collapse-content");
-    const iconButton = card.querySelector(".icon-button");
-    const icon = card.querySelector(".icon");
+  const collapsibleSections = document.querySelectorAll(".title1");
+  collapsibleSections.forEach(function (section) {
+    const collapseContent = section.nextElementSibling;
 
-    function toggleCollapse(event) {
-      //   event.stopPropagation(); // 阻止事件冒泡
+    function toggleCollapse() {
       collapseContent.classList.toggle("open");
-      icon.classList.toggle("rotate");
     }
 
-    iconButton.addEventListener("click", toggleCollapse);
+    section.addEventListener("click", toggleCollapse);
   });
 };
